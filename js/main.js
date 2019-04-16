1076581f5dc4b46abc65d23473ba42cf179f562a
MasafumiKabe <msfmkb@gmail.com>
2019-04-16 23:33:18 +0900

    update

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
   const question = document.getElementById('question');
   const btn = document.getElementById('btn');
   const choices = document.getElementById('choices');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
 
   const quizSet = [
     {q: 'What is A?', c: ['A0', 'A1', 'A2']},
@@ -11,6 +13,8 @@
     {q: 'What is C?', c: ['C0', 'C1', 'C2']},
   ];
   let currentNum = 0;
+  let isAnswered;
+  let score = 0;
 
   function shuffle(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
@@ -21,15 +25,29 @@
   }
 
   function checkAnswer(li) {
+    if (isAnswered) {
+      return;
+    }
+    isAnswered = true;
+
     if (li.textContent === quizSet[currentNum].c[0]) {
       li.classList.add('correct');
+      score++;
     } else {
       li.classList.add('wrong');
     }
+
+    btn.classList.remove('disabled');
   }
 
   function setQuiz() {
+    isAnswered = false;
+
     question.textContent = quizSet[currentNum].q;
+
+    while (choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
 
     const shuffledChoices = shuffle([...quizSet[currentNum].c]);
     shuffledChoices.forEach(choice => {
@@ -40,7 +58,26 @@
       });
       choices.appendChild(li);
     });
+
+    if (currentNum === quizSet.length - 1) {
+      btn.textContent = 'Show Score';
+    }
   }
 
   setQuiz();
+
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('disabled')) {
+      return;
+    }
+    btn.classList.add('disabled');
+
+    if (currentNum === quizSet.length - 1) {
+      scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+      result.classList.add('show');
+    } else {
+      currentNum++;
+      setQuiz();
+    }
+  });
 }
